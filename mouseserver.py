cad882183eff95e18374165ca076d5fc61d61417
Ankit Raj Mahapatra <54436840+Ankit404butfound@users.noreply.github.com>
2021-04-21 11:58:27 +0530

    Update mouseserver.py

--- mouseserver.py
+++ mouseserver.py
@@ -291,7 +291,7 @@ def ghasit_mouse():
     return "1"
 
 def main():
-    app.run("0.0.0.0",port=5000)
+    app.run("0.0.0.0",port=8000)
 ##    app.run(host="192.168.43.17", port=33)
 if __name__ == "__main__":
     main()
