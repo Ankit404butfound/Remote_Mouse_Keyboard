a9dace559b7afef58afec7b74c4e15039c04bcc6
aaryanrr <raiaryan2422@gmail.com>
2021-05-02 12:41:46 +0530

    Formatted the Files

--- mouseserver.py
+++ mouseserver.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask, request
 import pyautogui as p
 import logging
 import threading
@@ -12,17 +12,18 @@ moving = False
 drag = False
 type_data = ""
 old_data = ""
-coords = (0,0)
-lastcords = (0,0)
-lstmcord = (0,0)
+coords = (0, 0)
+lastcords = (0, 0)
+lstmcord = (0, 0)
 lstlen = 0
-coords = [(0,0)]
+coords = [(0, 0)]
 
-@app.route('/',methods=["GET","POST"])
+
+@app.route('/', methods=["GET", "POST"])
 def send():
-    #print("here")
-    if request.method=='POST':
-        ##print(request.get_json())
+    # print("here")
+    if request.method == 'POST':
+        # print(request.get_json())
         return "ok"
     return '''<!DOCTYPE html>
 <html>
@@ -195,61 +196,65 @@ function enter_but(){
   </body>
 </html>'''
 
-@app.route('/handler',methods=["POST"])
+
+@app.route('/handler', methods=["POST"])
 def handle():
     global moving, cordlst, lastcords
     lst = []
-    a=request.form['a']
-    b=request.form['b']
+    a = request.form['a']
+    b = request.form['b']
     a = float(a)
     b = float(b)
     moving = True
-    coords = (a,b)
-    lx,ly = lastcords
-    cx,cy = coords
-    #print(cx,cy)
-    #p.moveRel((cx-lx)/2,(cy-ly)/2)
-    threading.Thread(target=lambda:p.moveRel((cx-lx)*2,(cy-ly)*2)).start()
+    coords = (a, b)
+    lx, ly = lastcords
+    cx, cy = coords
+    # print(cx,cy)
+    # p.moveRel((cx-lx)/2,(cy-ly)/2)
+    threading.Thread(target=lambda: p.moveRel(
+        (cx - lx) * 2, (cy - ly) * 2)).start()
     lastcords = coords
     return "1"
 
-@app.route('/scroller',methods=["POST"])
+
+@app.route('/scroller', methods=["POST"])
 def scrollerr():
     global moving, cordlst, lastcords2
-    a=request.form['a']
-    b=request.form['b']
+    a = request.form['a']
+    b = request.form['b']
     a = float(a)
     b = float(b)
-    coords = (a,b)
-    lx,ly = lastcords2
-    cx,cy = coords
+    coords = (a, b)
+    lx, ly = lastcords2
+    cx, cy = coords
     if cy < ly:
-        threading.Thread(target=lambda:p.scroll(50)).start()
+        threading.Thread(target=lambda: p.scroll(50)).start()
     if cy > ly:
-        threading.Thread(target=lambda:p.scroll(-50)).start()
+        threading.Thread(target=lambda: p.scroll(-50)).start()
     lastcords2 = coords
     return "1"
-    
 
-@app.route("/tstart",methods=["POST"])
+
+@app.route("/tstart", methods=["POST"])
 def startt():
-    global lastcords,lastcords2
-    ##print("ened")
-    a=request.form['a']
-    b=request.form['b']
-    #print(a,b)
+    global lastcords, lastcords2
+    # print("end")
+    a = request.form['a']
+    b = request.form['b']
+    # print(a,b)
     a = float(a)
     b = float(b)
-    lastcords = (a,b)
-    lastcords2 = (a,b)
+    lastcords = (a, b)
+    lastcords2 = (a, b)
     return "1"
 
-@app.route("/click",methods=["POST"])
+
+@app.route("/click", methods=["POST"])
 def do_click():
     global moving
     if not moving:
-        a=request.form['a']
-        #print(a)
+        a = request.form['a']
+        # print(a)
         a = float(a)
         if a < 400:
             p.click()
@@ -259,40 +264,46 @@ def do_click():
     moving = False
     return "1"
 
-@app.route("/typed",methods=["POST"])
+
+@app.route("/typed", methods=["POST"])
 def typeit():
     global type_data, old_data
     data = request.form['data']
     type_data = str(data)
     if len(type_data) > len(old_data):
-        p.typewrite(type_data[len(type_data)-1])
+        p.typewrite(type_data[len(type_data) - 1])
     else:
-        p.press("backspace",len(old_data)-len(type_data))
+        p.press("backspace", len(old_data) - len(type_data))
     old_data = type_data
     return "1"
 
-@app.route("/enter",methods=["POST"])
+
+@app.route("/enter", methods=["POST"])
 def slashN():
     p.press("enter")
     return "1"
 
-@app.route("/dradhandler",methods=["POST"])
+
+@app.route("/dradhandler", methods=["POST"])
 def ghasit_mouse():
     global drag
-    #print(drag)
-    if drag == True:
+    # print(drag)
+    if drag:
         drag = False
         p.mouseUp()
-        
+
     else:
         drag = True
         p.mouseDown()
-        
+
     return "1"
 
+
 def main():
-    app.run("0.0.0.0",port=8000)
-##    app.run(host="192.168.43.17", port=33)
+    app.run("0.0.0.0", port=8000)
+
+
+# app.run(host="192.168.43.17", port=33)
 if __name__ == "__main__":
     main()
-#app.run(host='localhost')
+# app.run(host='localhost')
